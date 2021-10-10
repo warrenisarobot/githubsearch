cfc98cd0445abe7eea5e2c6c80bf01749d9a6bff
warrenisarobot <warrencheif@gmail.com>
2021-10-10 15:25:50 -0600

    Updated readme

--- command/command.go
+++ command/command.go
@@ -1,14 +1,28 @@
 package command
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 type CommandOptionValues struct {
-	Token                 string
+	TokenOverride         string
 	SearchType            string
 	Organization          string
 	MaxConcurrentRequests int
+}
+
+func (cov *CommandOptionValues) Token() string {
+	if cov.TokenOverride != "" {
+		return cov.TokenOverride
+	}
+	val := os.Getenv("GH_TOKEN")
+	if val != "" {
+		return val
+	}
+	val = os.Getenv("GITHUB_TOKEN")
+	return val
 }
 
 var searchCmd = &cobra.Command{}
@@ -19,7 +33,7 @@ func Generate(cov *CommandOptionValues) *cobra.Command {
 		Short: "Search for a thing",
 	}
 
-	rootCmd.PersistentFlags().StringVarP(&cov.Token, "token", "t", "", "Github auth token to use for searching")
+	rootCmd.PersistentFlags().StringVarP(&cov.TokenOverride, "token", "t", "", "Github auth token to use for searching.  If this is not provided the env vars GH_TOKEN, or GITHUB_TOKEN will be used if available")
 	rootCmd.PersistentFlags().IntVarP(&cov.MaxConcurrentRequests, "maxrequests", "m", 5, "Github auth token to use for searching")
 	rootCmd.AddCommand(SearchCommand(cov))
 	return rootCmd
